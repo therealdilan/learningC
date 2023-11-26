3a3a69e3cf29e43a66b96c8ddd2ed4a5b5a10deb
dilan <125643732+therealdilan@users.noreply.github.com>
2023-11-26 23:07:53 +0400

    shape display w/ basic input

--- graphics/output.cpp
+++ graphics/output.cpp
@@ -1,8 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+    float circleMoveSpeed = 0.000001; 
+
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Ermmmmm!");
+
+    sf::CircleShape circle(100);
+
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition(300, 300);
 
     while (window.isOpen())
     {
@@ -11,9 +20,21 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
+
+            if(event.key.code == sf::Keyboard::W) {
+                circleMoveSpeed = 0.5;
+            }
+
+            if(event.key.code == sf::Keyboard::S) {
+                circleMoveSpeed = 0.5;
+            }
+            
+        }   
+
+        circle.setPosition(circle.getPosition().x + circleMoveSpeed, circle.getPosition().y - circleMoveSpeed);
 
         window.clear();
+        window.draw(circle);
         window.display();
     }
 
